8ccb8cd2d88f6ffce73ce87fa88b74a22d288d76
Anton Babyak <a.babyak1@pravo.tech>
2020-07-07 17:46:55 +0400

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"><head>
     <link href="/css/main.css" rel="stylesheet">
-    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/materialize.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/js/materialize.js"></script>
     <script src="/js/main.js"></script>
